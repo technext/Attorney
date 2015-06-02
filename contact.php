bff2fb9212a340980840227aa5d4d00bdcfa8ef0
Mustafijur Rahman Nebir <nebir1993@gmail.com>
2015-06-02 16:13:31 +0600

    working contact form test

---
+++ contact.php
@@ -0,0 +1,23 @@
+<?php
+	$to      = 'nebir1993@gmail.com';
+	$email   = $_POST['email'];
+
+	$name  = $_POST['name'];
+	$subject = $_POST['subject'];
+	$message = $_POST['comments'];
+ 
+	$headers = 'From: http://themewagon.com/'. '<'.$email.'>' . "\r\n" .
+        'Reply-To: '. $email . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+	if(mail($to, $subject, $message, $headers)) {
+		echo "Your mail was sent successfully!";
+		echo "<script>window.location.href = 'http://technext.github.io/Attorney/';</script>";
+	}
+	else {
+		echo "Mail was not sent. Please try again.";
+	}
+
+	
+   
+?>
